0a9ae93e594d319265bae222d77fdceef648a72b
akane-aa <kagemiyasenn@gmail.com>
2019-09-06 23:58:05 +0900

    fix js

--- js/food.js
+++ js/food.js
@@ -1,4 +1,5 @@
 var jsbook = { title: "にんじん", price: 100, stock: 1 };
+var food = { title: "キャベツ", price: 110, stock: 1 };
 console.log(jsbook);
 console.log(jsbook.title);
 console.log(jsbook["price"]);
